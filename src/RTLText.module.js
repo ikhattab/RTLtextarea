68e00ec313c1d2ab0b07d248178cc0794f400d88
Ihab Khattab <ikhattab@cloud9ers.com>
2014-07-09 15:43:08 +0200

    added support for inputs and contenteditable

--- src/RTLText.module.js
+++ src/RTLText.module.js
@@ -15,57 +15,26 @@
  * call RTLText.setText(textarea, initial_string) to set markers on that
  * initial text.
  */
-var RTLText = function () {
-  'use strict';
-
+var RTLText = function() {
   var that = {};
   var rtlThreshold = 0.3;
-
-  /*
-   * Right-to-left Unicode blocks for modern scripts are:
-   *
-   * Consecutive range of the main letters:
-   * U+0590  to U+05FF  - Hebrew
-   * U+0600  to U+06FF  - Arabic
-   * U+0700  to U+074F  - Syriac
-   * U+0750  to U+077F  - Arabic Supplement
-   * U+0780  to U+07BF  - Thaana
-   * U+07C0  to U+07FF  - N'Ko
-   * U+0800  to U+083F  - Samaritan
-   *
-   * Arabic Extended:
-   * U+08A0  to U+08FF  - Arabic Extended-A
-   *
-   * Consecutive presentation forms:
-   * U+FB1D  to U+FB4F  - Hebrew presentation forms
-   * U+FB50  to U+FDFF  - Arabic presentation forms A
-   *
-   * More Arabic presentation forms:
-   * U+FE70  to U+FEFF  - Arabic presentation forms B
-   */
-  var rtlChar = /[\u0590-\u083F]|[\u08A0-\u08FF]|[\uFB1D-\uFDFF]|[\uFE70-\uFEFF]/mg;
+  var rtlChar = /[\u0600-\u06FF]|[\u0750-\u077F]|[\u0590-\u05FF]|[\uFE70-\uFEFF]/mg;
   var dirMark = /\u200e|\u200f/mg;
   var ltrMark = "\u200e";
   var rtlMark = "\u200f";
+  var rtlHashtag = /^#.*([\u0600-\u06FF]|[\u0750-\u077F]|[\u0590-\u05FF]|[\uFE70-\uFEFF])+/mg;
   var keyConstants = {
     BACKSPACE: 8,
     DELETE: 46
   };
   var twLength = 0;
-  var DEFAULT_TCO_LENGTH = 22;
-  var tcoLength = null;
+  var oldText = "";
+  var tcoLength = 20;
   var isRTL = false;
   var originalText = "";
   var originalDir = "";
   // Can't use trim cause of IE. Regex from here: http://stackoverflow.com/questions/2308134/trim-in-javascript-not-working-in-ie
   var trimRegex = /^\s+|\s+$/g;
-
-  var setManually = false;
-  var heldKeyCodes =  { '91': false,
-                        '16': false,
-                        '88': false,
-                        '17': false };
-  var useCtrlKey = navigator.userAgent.indexOf('Mac') === -1;
 
   /* Private methods */
 
@@ -82,6 +51,8 @@ var RTLText = function () {
 
   function getCaretPosition (el) {
     if (!elementHasFocus(el)) { return 0; }
+    // support for content editable
+    if (el.tagName !== "TEXTAREA" && el.tagName !== "INPUT") {el.value = el.textContent};
 
     var range;
     if (typeof el.selectionStart === "number") {
@@ -98,7 +69,6 @@ var RTLText = function () {
 
   function setCaretPosition (el, position) {
     if (!elementHasFocus(el)) { return; }
-
     if (typeof el.selectionStart === "number") {
       el.selectionStart = position;
       el.selectionEnd = position;
@@ -112,17 +82,18 @@ var RTLText = function () {
     }
   }
 
+  function getSelection () {
+    return window.getSelection ? window.getSelection().toString() : document.selection.createRange().text;
+  }
   // End of caret methods
 
   function replaceIndices (oldText, extractFn, replaceCb) {
     var lastIndex = 0;
     var newText = '';
-    var extractedItems = extractFn(oldText);
-
+    var extractedItems = extractFn(oldText)
     for (var i = 0; i < extractedItems.length; i++) {
       var item = extractedItems[i];
       var type = '';
-
       if (item.screenName) {
         type = 'screenName';
       }
@@ -132,15 +103,10 @@ var RTLText = function () {
       if (item.url) {
         type = 'url';
       }
-      if (item.cashtag) {
-        type = 'cashtag';
-      }
-
       var respObj = {
         entityText: oldText.slice(item.indices[0], item.indices[1]),
         entityType: type
       };
-
       newText += oldText.slice(lastIndex, item.indices[0]) + replaceCb(respObj);
       lastIndex = item.indices[1];
     }
@@ -162,18 +128,15 @@ var RTLText = function () {
         else if (itemObj.entityType === "url") {
           return itemObj.entityText + ltrMark;
         }
-        else if (itemObj.entityType === "cashtag") {
-          return ltrMark + itemObj.entityText;
-        }
       });
     }
     return text;
   }
 
   // If a user deletes a hidden marker char, it will just get rewritten during
-  // notifyTextUpdated. Special case this by continuing to delete in the same
+  // notifyTextUpdated.  Special case this by continuing to delete in the same
   // direction until a normal char is consumed.
-  function erasePastMarkers (e) {
+  function erasePastMarkers(e) {
     var offset;
     var textarea = (e.target) ? e.target : e.srcElement;
     var key = (e.which) ? e.which : e.keyCode;
@@ -184,7 +147,8 @@ var RTLText = function () {
     } else {
       return;
     }
-
+    // support for content editable
+    if (textarea.tagName !== "TEXTAREA" && textarea.tagName !== "INPUT") {textarea.value = textarea.textContent};
     var pos = getCaretPosition(textarea);
     var text = textarea.value;
     var numErased = 0;
@@ -198,7 +162,6 @@ var RTLText = function () {
         text = text.slice(0, pos) + text.slice(pos + 1, text.length);
       }
     } while (charToDelete.match(dirMark));
-
     if (numErased > 1) {
       textarea.value = text;
       // If more than 1 needed to be removed, update the text
@@ -209,7 +172,7 @@ var RTLText = function () {
   }
 
   function removeMarkers (text) {
-    return text ? text.replace(dirMark, '') : '';
+    return text.replace(dirMark, '');
   }
 
   function shouldBeRTL (plainText) {
@@ -217,67 +180,33 @@ var RTLText = function () {
     // Remove original placeholder text from this
     plainText = plainText.replace(originalText, "");
     var urlMentionsLength = 0;
-    var trimmedText = plainText.replace(trimRegex, '');
+    var trimmedText = plainText.replace(trimRegex,'');
     var defaultDir = originalDir;
 
-    if (!trimmedText || !trimmedText.replace(/#/g, '')) {
-      return defaultDir === 'rtl' ? true : false; // No text, use default.
+    if (!trimmedText || !trimmedText.replace(/#/g,'')) {
+      return defaultDir === 'rtl' ? true : false;  // No text, use default.
     }
 
     if (!matchedRtlChars) {
-      return false; // No RTL chars, use LTR
+      return false;  // No RTL chars, use LTR
     }
 
     if (plainText) {
       var mentions = twttr.txt.extractMentionsWithIndices(plainText);
       var mentionsLength = mentions.length;
-      var i;
-
-      for (i = 0; i < mentionsLength; i++) {
-        urlMentionsLength += mentions[i].screenName.length + 1;
-      }
-
+      for (var x = 0; x < mentionsLength; x++) {
+        var value = mentions[x];
+        urlMentionsLength += value.screenName.length + 1;
+      };
       var urls = twttr.txt.extractUrlsWithIndices(plainText);
       var urlsLength = urls.length;
-
-      for (i = 0; i < urlsLength; i++) {
-        urlMentionsLength += urls[i].url.length + 2;
-      }
-
+      for (var x = 0; x < urlsLength; x++) {
+        var value = urls[x];
+        urlMentionsLength += value.url.length + 2;
+      };
     }
     var length = trimmedText.length - urlMentionsLength;
     return length > 0 && matchedRtlChars.length / length > rtlThreshold;
-  }
-
-  function detectManualDirection (e) {
-    var textarea = e.target || e.srcElement;
-    if (e.type === "keydown" && (e.keyCode === 91 || e.keyCode === 16 || e.keyCode === 88 || e.keyCode === 17)) {
-      heldKeyCodes[String(e.keyCode)] = true;
-    }
-    else if (e.type === "keyup" && (e.keyCode === 91 || e.keyCode === 16 || e.keyCode === 88 || e.keyCode === 17)) {
-      heldKeyCodes[String(e.keyCode)] = false;
-    }
-
-    if (((!useCtrlKey && heldKeyCodes['91']) || (useCtrlKey && heldKeyCodes['17'])) && heldKeyCodes['16'] && heldKeyCodes['88']) {
-      setManually = true;
-
-      if (textarea.dir === 'rtl') {
-        setTextDirection('ltr', textarea);
-      }
-      else {
-        setTextDirection('rtl', textarea);
-      }
-      heldKeyCodes =  { '91': false,
-                        '16': false,
-                        '88': false,
-                        '17': false };
-    }
-  }
-
-  function setTextDirection (dir, textarea) {
-    textarea.setAttribute('dir', dir);
-    textarea.style.direction = dir;
-    textarea.style.textAlign = (dir === 'rtl' ? 'right' : 'left');
   }
 
   /* Public methods */
@@ -286,18 +215,15 @@ var RTLText = function () {
   that.onTextChange = function (e) {
     var event = e || window.event;
 
-    detectManualDirection(e);
-
     // Handle backspace through control characters:
     if (event.type === "keydown") {
       erasePastMarkers(event);
     }
-
     that.setText(event.target || event.srcElement);
-  };
-
+  }
   // Optionally takes a second param, with original text, to exclude from RTL/LTR calculation
-  that.setText = function (textarea) {
+  that.setText = function(textarea) {
+
     // Original directionality could be in a few places. Check them all.
     if (!originalDir) {
       if (textarea.style.direction) {
@@ -313,78 +239,61 @@ var RTLText = function () {
         originalDir = document.body.dir;
       }
     }
-
-    if (arguments.length === 2) {
+    if (arguments.length == 2) {
       originalDir = textarea.ownerDocument.documentElement.className;
       originalText = arguments[1];
     }
-
+    // support for content editable
+    if (textarea.tagName !== "TEXTAREA" && textarea.tagName !== "INPUT") {textarea.value = textarea.textContent};
     var text = textarea.value;
-    if (!text) {
-      return;
-    }
     var plainText = removeMarkers(text);
     isRTL = shouldBeRTL(plainText);
     var newText = setMarkers(plainText);
     var newTextDir = (isRTL ? 'rtl' : 'ltr');
 
     if (newText !== text) {
-      // Fix for Chrome for Android
-      textarea.value = "";
-      textarea.focus();
       textarea.value = newText;
       // Assume any recent change in text length due to markers affects the
       // current cursor position. If more accuracy is needed, the position
       // could be translated during replace operations inside setMarkers.
-      setCaretPosition(textarea, getCaretPosition(textarea) + newText.length - plainText.length);
+      pos = getCaretPosition(textarea);
+      setCaretPosition(textarea, pos + newText.length - plainText.length);
     }
-    if (!setManually) {
-      setTextDirection(newTextDir, textarea);
-    }
-  };
+    textarea.setAttribute('dir', newTextDir);
+    textarea.style.direction = newTextDir;
+    textarea.style.textAlign = (newTextDir === 'rtl' ? 'right' : 'left');
+  }
 
   // Use this to get the length of a tweet with unicode control characters removed
-  that.textLength = function (text) {
+  that.textLength = function(text) {
     var tweet = removeMarkers(text);
     var urls = twttr.txt.extractUrls(tweet);
     var length = tweet.length - urls.join('').length;
     var urlsLength = urls.length;
-    var tcoLength = that.getTcoLength();
     for (var i = 0; i < urlsLength; i++) {
       length += tcoLength;
       if (/^https:/.test(urls[i])) {
         length += 1;
       }
-    }
+    };
 
     return twLength = length;
-  };
+  }
 
   // Do this before text is submitted
-  that.cleanText = function (text) {
+  that.cleanText = function(text) {
     return removeMarkers(text);
-  };
+  }
 
   // If markers need to be added to a string without affecting the text box, use this
   that.addRTLMarkers = function (s) {
     return setMarkers(s);
-  };
+  }
 
   // For determining if text should be RTL (returns boolean)
-  that.shouldBeRTL = function (s) {
+  that.shouldBeRTL = function(s) {
     return shouldBeRTL(s);
-  };
-
-  that.getTcoLength = function () {
-    return tcoLength || DEFAULT_TCO_LENGTH;
-  };
-  that.setTcoLength = function (length) {
-    if (length > 0) {
-      tcoLength = parseInt(length, 10);
-    } else {
-      tcoLength = null;
-    }
-  };
+  }
 
   return that;
 
@@ -393,4 +302,3 @@ var RTLText = function () {
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = RTLText;
 }
-
